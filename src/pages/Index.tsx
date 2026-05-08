f81e10484424bfabf2418a52b6a92abc884a39e2
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-05-08 16:58:14 +0545

    update Index.tsx.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,7 @@ import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import GitHubStats from "@/components/GitHubStats";
+
 //import Certifications from "@/components/Certifications";
 //import Testimonials from "@/components/Testimonials";
 //import BlogHighlight from "@/components/BlogHighlight";
@@ -22,9 +23,11 @@ const Index = () => {
       <Experience />
       <Projects />
       <GitHubStats />
+
       {/*  <Certifications /> */}     {/* NEW: Shows your credentials */}
       {/* <Testimonials />  */}      {/* NEW: Social proof */}
       {/* <BlogHighlight /> */}   {/* NEW: Shows technical writing */}
+      
       <Contact />
       <Footer />
     </div>
