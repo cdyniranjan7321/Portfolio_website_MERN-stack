3bd828798717c9c9ef27d6c57bf74a6fda233ab5
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-06-01 17:34:46 +0545

    update index.tsx files.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -31,8 +31,10 @@ const Index = () => {
       
       <Contact />
       <Footer />
+      
          {/* Add the chatbot here - it will appear on all pages */}
       <PortfolioChatBot />
+      
     </div>
   );
 };
