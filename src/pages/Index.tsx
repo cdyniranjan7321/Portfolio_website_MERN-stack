3719aa924521bdfb9147bd42075c5f760bc9af9b
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-06-21 18:06:43 +0545

    update visitorCounter.tsx page.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -29,7 +29,6 @@ const Index = () => {
       <div className="container mx-auto px-4 py-8 flex justify-center">
         <VisitorCounter />
       </div>
-
       {/*  <Certifications /> */}     {/* NEW: Shows your credentials */}
       {/* <Testimonials />  */}      {/* NEW: Social proof */}
       {/* <BlogHighlight /> */}   {/* NEW: Shows technical writing */}
