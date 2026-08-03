b0066b7ce50e267179b877bf185a19dee107bfcc
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-08-03 12:00:12 +0545

    update blog&consultancy image section.

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -360,6 +360,12 @@ const projects = [
         title: "Blog & Consultancy Homepage",
         description: "Blog & Consultancy Homepage with featured articles and services."
       },
+      {
+        src: "/projects/blog&consultancy/servicespage.png",
+        alt: "Services Page",
+        title: "Services Page",
+        description: "Services Page with detailed descriptions of consultancy offerings."
+      },
     ]
   },
 
