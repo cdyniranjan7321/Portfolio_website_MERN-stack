15f06092289b689fd05c46e2f0673ba8bf5be9e1
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-08-21 10:38:14 +0545

    change CaseStudy page to add LMS projects new image

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -669,7 +669,7 @@ const projects = [
         src: "/projects/lms/signuppageforstudent&instructor.png",
         alt: "Ridgeline LMS Website Signup Page for Both Student and Instructor.",
         title: "Ridgeline LMS Website Signup Page for Both Student and Instructor.",
-        description: "Contact Page "
+        description: "Signup Page for as a Student or Instructor. "
        },
       ]
     }
