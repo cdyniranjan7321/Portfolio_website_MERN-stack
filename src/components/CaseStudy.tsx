0bc6d4e884ea48684c6b31c008994a1312388200
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-08-21 16:02:38 +0545

    change CaseStudy page to add new images in LMS projects.

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -683,6 +683,12 @@ const projects = [
         title: "Student Dashboard Course Detail Page",
         description: "Student Dashboard Course Detail Page with all about course, instructor information and payment section which helps to enroll to unlock video lessons."
        },
+        {
+        src: "/projects/lms/instructordashboard.png",
+        alt: "",
+        title: "",
+        description: ""
+       },
       ]
     }
 ];
