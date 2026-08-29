f418a44cd52fecf63b0da456e13fa7ed82191201
cdyniranjan7321 <cdyniranjan7321@gmail.com>
2026-08-29 09:12:56 +0545

    Update CaseStudy page to add new image

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -696,10 +696,10 @@ const projects = [
         description: "Create New course Page with all course details, category, level, price etc."
        },
        {
-        src: "/projects/lms/createnewcourse.png",
-        alt: "Create New course Page",
-        title: "Create New course Page",
-        description: "Create New course Page with all course details, category, level, price etc."
+        src: "/projects/lms/managecoursepage1.png",
+        alt: "Manage Course Page",
+        title: "Manage Course Page",
+        description: "Manage Course Page with all course details and add video lesson functions."
        },
       ]
     }
